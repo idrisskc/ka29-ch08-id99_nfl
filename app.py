6b29bf7fe18216fd4d11dc7466409df45946077a
idrisskc <70375313+idrisskc@users.noreply.github.com>
2025-10-18 19:42:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -276,7 +276,7 @@ div[data-testid="stMetricValue"] {{
 """, unsafe_allow_html=True)
 
 .kpi-card:hover {{
-    transform: translateY(1px);
+    transform: translateY(-2px);
     border-color: rgba(255,215,0,0.4);
 }}
 .kpi-title {{ 
